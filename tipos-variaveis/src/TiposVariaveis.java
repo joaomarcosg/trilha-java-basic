79db8b57e4b9ad3260e5c1e4db9fab027d4c0fcf
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-02-06 16:53:53 -0300

    exemplo de tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //double salarioMinimo = 2500;
+
+        //short numeroCurto = 1;
+        //int numeroNormal = numeroCurto;
+        //short numeroCurto2 = (short) numeroNormal;
+
+        final double PI_VALUE = 3.14;
+        System.out.print(PI_VALUE);
+
+
     }
 }
